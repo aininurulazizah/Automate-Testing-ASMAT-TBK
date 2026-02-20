f894f99af93eedf080c7fab763a698074cc3e044
aininurulazizah <aini.nurul.tif421@polban.ac.id>
2026-02-20 15:50:39 +0700

    Minor fix on laporan keseluruhan data test

--- tests/data/laporan_keseluruhan_data.js
+++ tests/data/laporan_keseluruhan_data.js
@@ -87,9 +87,58 @@ export const testData = {
         Layanan: 'Semua',
 
         KolomPendapatan: {
-
+            OmzetPenumpang: [
+                'omzet_penumpang'
+            ],
+            OmzetPaket: [
+                'omzet_paket_reguler',
+                'omzet_paket_corporate'
+                // 'omzet_paket_total'
+            ]
         },
         KolomPengeluaran: {
+            BiayaOp: [
+                'biaya_op_test',
+                'biaya_op_bbm_cash',
+                'biaya_op_bbm_emoney',
+                'biaya_op_etoll',
+                'biaya_op_op_freelance',
+                'biaya_op_op_karyawan'
+            ], 
+            Discount: [
+                'discount_discount',
+                'discount_discount_promo',
+                'discount_discount_voucher'
+            ]
+        },
+        KolomNonMonetary: {
+            JmlPenumpang: [
+                'jml_penumpang_go_show',
+                'jml_penumpang_offline',
+                'jml_penumpang_online'
+            ],
+            JmlPenumpangByPembayaran: [
+                'jml_penumpang_by_pembayaran_tun',
+                'jml_penumpang_by_pembayaran_edc',
+                'jml_penumpang_by_pembayaran_bni',
+                'jml_penumpang_by_pembayaran_vcr',
+                'jml_penumpang_by_pembayaran_trf',
+                'jml_penumpang_by_pembayaran_mbt',
+                'jml_penumpang_by_pembayaran_wl_and',
+                'jml_penumpang_by_pembayaran_wl_ios',
+                'jml_penumpang_by_pembayaran_wl_web',
+                'jml_penumpang_by_pembayaran_mer',
+                'jml_penumpang_by_pembayaran_ttx',
+                'jml_penumpang_by_pembayaran_trv',
+                'jml_penumpang_by_pembayaran_rdb',
+                'jml_penumpang_by_pembayaran_kiosk',
+                'jml_penumpang_by_pembayaran_esb',
+                'jml_penumpang_by_pembayaran_bca'
+            ],
+            JmlPaket: [
+                'jml_paket_reguler',
+                'jml_paket_corporate'
+            ]
 
         }
     },
