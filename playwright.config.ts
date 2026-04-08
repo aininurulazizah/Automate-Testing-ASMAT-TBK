ed14416dbb6cc73b462eb35eb96a9f5e978aa56a
aininurulazizah <aini.nurul.tif421@polban.ac.id>
2026-04-08 11:25:42 +0700

    Add json reporter on playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['list']],
+  reporter: [
+    ['html', { open: 'never' }], 
+    ['list'],
+    ['json', {outputFile: 'result.json'}]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
